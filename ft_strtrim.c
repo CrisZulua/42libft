40540d96b729bc6fc64c806f69655d90be6cc607
Cristian Augusto Zuluaga Posso <czuluaga@c1r8s5.42malaga.com>
2026-04-21 12:05:16 +0200

    strtrim working

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: czuluaga <czuluaga@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 08:50:56 by czuluaga          #+#    #+#             */
-/*   Updated: 2026/04/21 10:16:09 by czuluaga         ###   ########.fr       */
+/*   Updated: 2026/04/21 12:02:16 by czuluaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,36 @@ static int	ft_chrinset(const char *c, char const *set)
 		set++;
 	}
 	return (0);
+}
+
+static size_t	ft_trim_start(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+
+	end = ft_strlen(s1);
+	if (!end)
+		return (0);
+	start = 0;
+	while (ft_chrinset(&s1[start], set) && start < end)
+		start++;
+	return (start);
+}
+
+static size_t	ft_trim_end(char const *s1, char const *set, size_t start)
+{
+	long	end_trim;
+	long	end;
+	long	lstart;
+
+	lstart = (long)start;
+	end = ft_strlen(s1);
+	if (!end)
+		return (0);
+	end_trim = end - 1;
+	while (ft_chrinset(&s1[end_trim], set) && end_trim >= lstart)
+		end_trim--;
+	return (end_trim + 1);
 }
 
 /*
@@ -40,22 +70,15 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	end;
 	char	*trim_str;
 
-	start = 0;
-	end = ft_strlen(s1);
-	while (ft_chrinset(s1, set))
-		start++;
-	if (start >= end)
-		return ((char *)ft_calloc(1, sizeof(char)));
-	end--;
-	while (ft_chrinset(&s1[end], set))
-		end--;
+	start = ft_trim_start(s1, set);
+	end = ft_trim_end(s1, set, start);
 	trim_str = (char *)malloc((end - start + 1) * sizeof(char));
 	if (!trim_str)
 		return (NULL);
 	i = 0;
-	while (i <= end - start)
+	while (i < end - start)
 	{
-		trim_str[i] = s1[start + i];
+		trim_str[i] = s1[i + start];
 		i++;
 	}
 	trim_str[i] = 0;
